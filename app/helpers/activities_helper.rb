9d39debf3f78dc8a2dea6032e958316f17f402c3
9thScientist <bruno.ms.cancelinha@gmail.com>
2017-09-21 16:36:20 +0100

    minor changes

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,5 +1,9 @@
 module ActivitiesHelper
   def can_create?
-     can? :create, Activity
+    can? :create, Activity
+  end
+
+  def error_messages(activity)
+    activity.errors.full_messages
   end
 end
